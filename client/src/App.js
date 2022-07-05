12e1f240398da1fc297a9bfe46ce0ac62a8fa857
sacylkowski <sacylkowski@gmail.com>
2022-07-04 21:20:24 -0500

    initial file structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    // <Router>
+     <h1>Tavern Masters!</h1>
+    // </Router>
+  );
+}
+
+export default App;
