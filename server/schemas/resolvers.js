289243d1cdb3a52e3b0a5a48d4950579d887d595
erockenmeyer <erikrockenmeyer@gmail.com>
2022-07-07 20:53:41 -0500

    added mutation resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -47,16 +47,68 @@ const resolvers = {
         events: async (parent, {username}) => {
             const params = username ? { username } : {};
             return Event.find(params).sort({createdAt: -1});
-        }
+        },
 
         // get single event
         oneEvent: async (parent, { _id }) => {
             return Event.findOne({ _id });
-        }
+        },
     },
-    // Mutation: {
+    Mutation: {
+        // add a user
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+        },
+        // log in
+        login: async (parent, { username, password }) => {
+            const user = await User.findOne({ username });
 
-    // }
+            if (!user) {
+                throw new AuthenticationError('Username or password is incorrect.');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Username or password is incorrect.');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        // add campaign
+        addCampaign: async (parent, args, context) => {
+            if (context.user) {
+                const campaign = await Campaign.create({ ...args, username: context.user.username });
+
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { campaigns: campaign._id }},
+                    { new: true }
+                );
+
+                return campaign;
+            }
+            throw new AuthenticationError('You need to be logged in to make a campaign!');
+        },
+        // add event
+        addEvent: async (parent, args, context) => {
+            if (context.user) {
+                const event = await Event.create({ ...args, username: context.user.username });
+
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { events: event._id }},
+                    { new: true }
+                );
+
+                return event;
+            }
+
+            throw new AuthenticationError('You need to be logged in to make an event!');
+        }
+    }
 };
 
 module.exports = resolvers;
