1d8aa350d936743fad82bb742f79c9b624b05461
alu1868 <alu1868@gmail.com>
2022-07-12 16:55:07 -0500

    installed @apollo/client + linked apollo client to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,42 +10,70 @@ import Profile from "./pages/Profile";
 import NoMatch from "./pages/NoMatch";
 import SingleCampaign from "./pages/SingleCampaign";
 
+// ApolloClient Link -- BEGIN --
+  import {ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from,} from '@apollo/client'
+  import { onError } from '@apollo/client/link/error'
+
+  // in case client runs into error connecting to GraphQl
+  const errorLink = onError(({ graphqlErrors, networkError }) => {
+    if (graphqlErrors) {
+      graphqlErrors.map(({ message, location, path }) => {
+        alert(`Graphql error ${message}`);
+      });
+    }
+  });
+
+  const link = from([
+    errorLink,
+    new HttpLink({ uri: "http://localhost:3001/graphql" }),
+  ]);
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: link
+  })
+
+// ApolloClient Link -- END --
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route
-            path="/home"
-            element={<Home />}
-          />
-          <Route
-            path="/login"
-            element={<Login />}
-          />
-           <Route
-            path="/signup"
-            element={<Signup />}
-          />
-          <Route
-            path="/profile"
-            element={<Profile />}
-          />
-          <Route
-              path="/campaign/:id"
-              element={<SingleCampaign />}
-            />
-          <Route
-            path="*"
-            element={<NoMatch />}
-          />
-        </Routes>
-      </div >
-      <Footer />
-    </Router>
+    // linking the apollo client to graphql api
+    <ApolloProvider client={client}>
 
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route
+              path="/home"
+              element={<Home />}
+            />
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+            <Route
+              path="/signup"
+              element={<Signup />}
+            />
+            <Route
+              path="/profile"
+              element={<Profile />}
+            />
+            <Route
+                path="/campaign/:id"
+                element={<SingleCampaign />}
+              />
+            <Route
+              path="*"
+              element={<NoMatch />}
+            />
+          </Routes>
+        </div >
+        <Footer />
+      </Router>
+      
+    </ApolloProvider>
   );
 }
 
