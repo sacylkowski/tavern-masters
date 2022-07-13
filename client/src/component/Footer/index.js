1d3e66fdcf453a8aa4e22b39ef02253650529bfd
erockenmeyer <erikrockenmeyer@gmail.com>
2022-07-12 20:13:54 -0500

    added erik portfolio link

--- client/src/component/Footer/index.js
+++ client/src/component/Footer/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <footer className="">
-            <p><strong>&copy;2022 Campaign Generator</strong> by <Link to={{ pathname: "" }} target="_blank">Erik Rockenmeyer</Link>,
+            <p><strong>&copy;2022 Campaign Generator</strong> by <Link to={{ pathname: "https://erockenmeyer.github.io/Portfolio" }} target="_blank">Erik Rockenmeyer</Link>,
             <Link to={{ pathname: "https://sacylkowski.github.io/react-portfolio/" }} target="_blank">Sarah Cylkowski</Link>, <Link to={{ pathname: "" }} target="_blank">Alexander Lu</Link>,
             <Link to={{ pathname: "" }} target="_blank">Thomas Ruppeniner</Link>, and <Link to={{ pathname: "" }} target="_blank">Grace Marcoux</Link>.
             </p>
