6e9c6834f9baf18e1560a5c4f3332c34a9ad8c8e
erockenmeyer <erikrockenmeyer@gmail.com>
2022-07-12 20:31:30 -0500

    capitalized Encounter as necessary in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,7 +4,7 @@ const typeDefs = gql`
     type User {
         _id: ID
         username: String
-        encounters: [encounter]
+        encounters: [Encounter]
         encounterCount: Int
         campaignCount: Int
         campaigns: [Campaign]
@@ -17,11 +17,11 @@ const typeDefs = gql`
         campaignDescription: String
         encounterCount: Int
         commentCount: Int
-        encounters: [encounter]
+        encounters: [Encounter]
         comments: [Comment]
     }
 
-    type encounter {
+    type Encounter {
         _id: ID
         username: String
         encounterGiverName: String
@@ -45,14 +45,14 @@ const typeDefs = gql`
         user(username: String!): User
         campaigns(username: String): [Campaign]
         campaign(_id: ID!): Campaign
-        encounters(username: String): [encounter]
-        encounter(_id: ID!): encounter
+        encounters(username: String): [Encounter]
+        encounter(_id: ID!): Encounter
     }
 
     type Mutation {
         login(username: String!, password: String!): Auth
         addUser(username: String!, password: String!): Auth
-        addEncounter(encounterGiverName: String!, encounterGiverOccupation: String, encounterGiverDescription: String, encounterName: String!, encounterDescription: String!, encounterReward: String): encounter
+        addEncounter(encounterGiverName: String!, encounterGiverOccupation: String, encounterGiverDescription: String, encounterName: String!, encounterDescription: String!, encounterReward: String): Encounter
         addCampaign(campaignName: String!, campaignDescription: String!): Campaign
         updateCampaign(id: ID!, campaignName: String!, campaignDescription: String!): Campaign
         addEncounterCampaign(campaignId: ID!, encounterId: ID!): Campaign
