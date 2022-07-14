dcb0048ca318d2faf67296a62e777e8a591aecc7
alu1868 <alu1868@gmail.com>
2022-07-14 16:24:39 -0500

    added css sheet

--- client/src/component/CampaignList/index.js
+++ client/src/component/CampaignList/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./campaignlist.css";
 
 const CampaignList = ({ campaigns, title }) => {
     if (campaigns.length) {
@@ -7,7 +8,7 @@ const CampaignList = ({ campaigns, title }) => {
     }
 
     return(
-        <div>
+        <div className="card-Body">
             {/* the campaign name, the title */}
             <h3>{title}</h3>
             {/* then it will map through the campaigns and show the description and who created it */}
@@ -15,7 +16,7 @@ const CampaignList = ({ campaigns, title }) => {
                 <div>
                     <h4 className="card-header">{campaigns.campaignName}</h4>
                     
-                    <p className="card-Content">{campaigns.campaignDescription}</p>
+                    <p className="card-content">{campaigns.campaignDescription}</p>
 
                     <p className="card-footer">
                         <Link
