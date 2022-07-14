d2beb54360678d03b17b39814f52128914934098
Grace Marcoux <gmmarcoux@me.com>
2022-07-14 11:06:55 -0500

    ftr

--- client/src/component/Footer/index.js
+++ client/src/component/Footer/index.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer className="">
             <p><strong>&copy;2022 Campaign Generator</strong> by <Link to={{ pathname: "https://erockenmeyer.github.io/Portfolio" }} target="_blank">Erik Rockenmeyer</Link>,
             <Link to={{ pathname: "https://sacylkowski.github.io/react-portfolio/" }} target="_blank">Sarah Cylkowski</Link>, <Link to={{ pathname: "" }} target="_blank">Alexander Lu</Link>,
-            <Link to={{ pathname: "" }} target="_blank">Thomas Ruppeniner</Link>, and <Link to={{ pathname: "" }} target="_blank">Grace Marcoux</Link>.
+            <Link to={{ pathname: "" }} target="_blank">Thomas Ruppeniner</Link>, and <Link to={{ pathname: "https://github.com/gmmarcoux" }} target="_blank">Grace Marcoux</Link>.
             </p>
         </footer>
     );
